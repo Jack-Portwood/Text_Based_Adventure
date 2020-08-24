def9e3ed6f1379f1ed2b40e1add3501565108cc0
Jack Portwood <Jack.Portwood14@gmail.com>
2020-08-24 11:14:24 +0100

    intro and start of game work

---
+++ textBaseAdventure.py
@@ -0,0 +1,25 @@
+#Setup 
+decision = ["yes", "no"]
+direction = ["forwards", "backwards", "left", "right"]
+
+#Introduction 
+print(" You are travelling down a wooded road..\n an old crone waves you down...")
+name = raw_input("Crone: Greetings traveler, what is your name?\n")
+print( "Salutations, " +name+ ".")
+
+
+
+
+
+
+
+#Start of Game
+response = ""
+while response not in decision:
+    print("Crone: I need you to retrieve me my stolen diadem from the mountain Queen..")
+    print("Crone: I will reward you with immeasurable wealth and fortune... ")
+    response = raw_input( "Do you accept ? \n yes/no \n")
+    if response == "yes":
+        print("The crown speaks in tounge and with a flick of the wrist you are stood in bog at the foot of a mountain")
+    else:
+        print("The crown stabs you with a blunt rusty blade... you are dead.")
