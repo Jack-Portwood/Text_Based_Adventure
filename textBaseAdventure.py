9b994578c3527c3883b99b32cea93083bbd05a6e
Jack Portwood <Jack.Portwood14@gmail.com>
2020-08-24 15:11:25 +0100

    Into the mine

--- textBaseAdventure.py
+++ textBaseAdventure.py
@@ -27,7 +27,7 @@ while response not in direction:
     print("To your right in the distance there is what appears dead forest.. \n")
     print("To your left in the deistance there is a rise in the ground.. \n")
     print("Behind you lies bog as far as the eye can see..\n")
-    response = raw_input("Which direction do you choose? \n forwards \n backwards \n right \n left\n ")
+    response = raw_input("Which direction do you choose? \n forwards \n right \n left\n ")
     if response == "forwards":
         print(""+name+ " trudged through the dank bog for days, the mountain did no appear any closer," +name+ "succumb to exhaustion and was consumed by the bog.")
         quit("Game Over...\n and thus draws to its natural conclusion the tale of " +name+ " the intrepid ")
@@ -37,8 +37,25 @@ while response not in direction:
     elif response == "left":
         print("You walk to the point of exhaustion but you make it to the rised ground.. you pass out..")
     elif response == "right":
-        print("You make it to the dead trees.. just you pass out..")
+        print("You make it to the dead trees... the ground becomes boggier and boggier, you are consumed by the bog")
+        quit("Game Over...\n and thus draws to its natural conclusion the tale of "  +name+ " the intrepid ")
+
 
 #Dream 
 print("Crone: I used to be the Princess of these once far lands.. but that wicked Queen took my throne.. please help restore my youth and lands.")
 print("You awaken!")
+
+#Level Two
+response = ""
+while response not in decision:
+    response = raw_input("You bring yourself to your feet..\n you notice a man whole.. do you decend into the darkness \n yes/no \n")
+if response == "yes":
+    print("You climb down the ladder into the darkness....")
+else:
+    print("You await the author the write something......")
+    quit("Game Over...\n and thus draws to its natural conclusion the tale of "  +name+ " the intrepid ")
+
+
+
+
+
